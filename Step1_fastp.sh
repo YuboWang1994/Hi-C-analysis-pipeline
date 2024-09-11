cd901b92fe078e096c4cfa36b144c9ca8bce9e2f
YuboWang1994 <84012823+YuboWang1994@users.noreply.github.com>
2024-09-11 11:44:39 +0800

    Update Step1_fastp.sh

--- Step1_fastp.sh
+++ Step1_fastp.sh
@@ -1,3 +1,3 @@
 # Step 1 Obtain clean data for each sample
 
-{pipeline_path}/scripts/fastp -i {sample_raw_R1_data} -I {sample_raw_R2_data} -o {sample}.R1.clean.fq.gz -O {sample}.R2.clean.fq.gz -w 4 -j {sample}.json -h {sample}.html
+{pipeline_path}/scripts/fastp -i {sample_raw_R1_data} -I {sample_raw_R2_data} -o {sample}_R1.fastq.gz -O {sample}_R2.fastq.gz -w 4 -j {sample}.json -h {sample}.html
